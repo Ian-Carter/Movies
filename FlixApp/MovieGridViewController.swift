da96acc9e9dfb680749dc878ee684e82380ecced
Ian Carter <ianbricecarter@gmail.com>
2020-09-14 23:33:12 -0400

    Flix part 2

---
+++ FlixApp/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  FlixApp
+//
+//  Created by Ian Carter on 9/14/20.
+//  Copyright © 2020 Ian Carter. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
